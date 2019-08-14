0a472bbac3a5512426b791a07b69dd51a4a1c03c
Matt Williams <matt.seongjun@gmail.com>
2019-08-14 09:34:36 -0400

    Done

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,28 @@
 # Implement your procedural solution here!
+
+def multiple(n, m)
+    n % m == 0
+end
+
+def multiple_range(n, start, stop)
+    if start == stop && multiple(n, stop)
+        return true
+    elsif multiple(n, start)
+        return multiple_range(n, start + 1, stop)
+    else 
+        return false
+    end
+end
+
+def smallest_multiple(range)
+    i = range[1]
+    until multiple_range(i, range[0], range[1])
+        i += 1
+    end
+    return i
+end
+
+puts multiple_range(2520, 1, 10)
+puts smallest_multiple([1, 10])
+puts smallest_multiple([1, 12])
+puts smallest_multiple([1, 20])
